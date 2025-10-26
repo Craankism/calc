d975341cce6790088ea5dd26b4cf2d1539dfc873
Craankism <saschaschwellnus@gmail.com>
2025-10-26 11:51:44 +0100

    feat: add calculator base and automated workflow

--- main.go
+++ main.go
@@ -5,5 +5,28 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	var num1, num2 float64
+	var operator string
+	var result float64
+
+	fmt.Scanf("%f %s %f", &num1, &operator, &num2)
+
+	switch operator {
+		case "+":
+			result = num1 + num2
+		case "-":
+			result = num1 - num2
+		case "*":
+			result = num1 * num2
+		case "/":
+			result = num1 / num2
+			if num2 == 0 {
+				fmt.Println("Error: Division by zero")
+				return
+			}
+		default:
+			fmt.Println("Error: Unknown operator")
+			return
+	}
+	fmt.Printf("Result: %g\n", result)
 }
