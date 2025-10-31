de135c82b1682a18d0f972163720ff11c3d71e58
Craankism <saschaschwellnus@gmail.com>
2025-10-31 18:35:13 +0100

    fix: loop for endless calculation with result

--- main.go
+++ main.go
@@ -8,36 +8,40 @@ func main() {
 	var num1, num2 float64
 	var operator string
 	var result float64
+	firstRun := true
 
-	//Input
-	//first number
-	fmt.Println("Enter first number: ")
-	fmt.Scan(&num1)
-	//operator
-	fmt.Println("Enter operator: ")
-	fmt.Scan(&operator)
-	//second number
-	fmt.Println("Enter second number: ")
-	fmt.Scan(&num2)
-	
+	for {
+		//first number or previous result
+		if firstRun { 
+		fmt.Println("Enter number: ")
+		fmt.Scan(&num1)
+		firstRun = false
+		} else {
+			num1 = result
+		}
+		//operator
+		fmt.Println("Enter operator: ")
+		fmt.Scan(&operator)
+		//second number
+		fmt.Println("Enter number: ")
+		fmt.Scan(&num2)
+		
 
-	//Operator check and calculation
-	switch operator {
-		case "+":
-			result = num1 + num2
-		case "-":
-			result = num1 - num2
-		case "*":
-			result = num1 * num2
-		case "/":
-			result = num1 / num2
-			if num2 == 0 {
-				fmt.Println("Error: Division by zero")
-				return
-			}
-		default:
-			fmt.Println("Error: Unknown operator")
-			return
+		//Operator check and calculation
+		switch operator {
+			case "+":
+				result = num1 + num2
+			case "-":
+				result = num1 - num2
+			case "*":
+				result = num1 * num2
+			case "/":
+				result = num1 / num2
+				if num2 == 0 {
+					fmt.Println("Error: Division by zero")
+					continue
+				}
+		}
+		fmt.Printf("Result: %g\n", result)
 	}
-	fmt.Printf("Result: %g\n", result)
 }
