3de26c9e7799d94ba9a70fcbd98d7570677333e4
Craankism <saschaschwellnus@gmail.com>
2025-11-15 20:20:40 +0100

    feat: add simple calculation (#42)
    * feat: add parser to build stack
    
    * feat: add simple calculation

--- main.go
+++ main.go
@@ -25,15 +25,46 @@ package main
 // 	Result float64 `json:"result"`
 // }
 
-// func userInput(input quickmaffs) calcResponse {
-// 	calcResponseObject := calcResponse{
-// 		Result: 69,
-// 	}
+var lastTerm string
+
+func userInput(input quickmaffs) calcResponse {
+	calcResponseObject := calcResponse{
+		Result: 69,
+	}
+
+	fmt.Println("Input: ", input.Term)
+	fmt.Println("Response: ", calcResponseObject)
+	return calcResponseObject
+}
 
 // 	fmt.Println(input.Term)
 // 	return calcResponseObject
 // }
 
+	calcHandler := func(w http.ResponseWriter, r *http.Request) {
+		var qm quickmaffs
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+		err = json.Unmarshal(body, &qm)
+		if err != nil {
+			fmt.Println(err)
+		}
+		//parser to calculate
+		lastTerm = qm.Term
+		calculate()
+		
+		data, err := json.Marshal(userInput(qm))
+		if err != nil {
+			fmt.Println(err)
+		}
+		_, err = w.Write(data)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	http.HandleFunc("/calc", calcHandler)
 
 // func main() {
 // 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -64,14 +95,7 @@ package main
 // 	}
 // 	http.HandleFunc("/calc", calcHandler)
 
-// 	err := http.ListenAndServe(":8080", nil)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-
-// }
-
-// 
+//
 // func main() {
 // 	var num1, num2, result big.Float
 // 	var operator string
