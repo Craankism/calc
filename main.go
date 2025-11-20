e6a6b8a226f0845c8f1da4631859ecc9005b0afc
Craankism <saschaschwellnus@gmail.com>
2025-11-20 21:14:33 +0100

    feat: display result on html body, center ui and introduce . in parser for comma digits (#56)

--- main.go
+++ main.go
@@ -27,50 +27,50 @@ type calcResponse struct {
 
 var lastTerm string
 
-func userInput(input quickmaffs) calcResponse {
+func userInput(input quickmaffs, result float64) calcResponse {
 	calcResponseObject := calcResponse{
-		Result: 69,
+		Result: result,
 	}
 	fmt.Println("Input: ", input.Term)
 	fmt.Println("Response: ", calcResponseObject)
 	return calcResponseObject
-	}
+}
 
- func main() {
- 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
- 		http.ServeFile(w, r, "index.html")
- 	})
- 	http.HandleFunc("/static/style.css", func(w http.ResponseWriter, r *http.Request) {
- 		http.ServeFile(w, r, "static/style.css")
- 	})
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+	http.HandleFunc("/static/style.css", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/style.css")
+	})
 
- 	calcHandler := func(w http.ResponseWriter, r *http.Request) {
- 		var qm quickmaffs
- 		body, err := io.ReadAll(r.Body)
- 		if err != nil {
- 			fmt.Println(err)
- 		}
- 		err = json.Unmarshal(body, &qm)
- 		if err != nil {
- 			fmt.Println(err)
- 		}
+	calcHandler := func(w http.ResponseWriter, r *http.Request) {
+		var qm quickmaffs
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+		err = json.Unmarshal(body, &qm)
+		if err != nil {
+			fmt.Println(err)
+		}
 		//parser to calculate
 		lastTerm = qm.Term
-		calculate()
+		result := calculate()
 
- 		data, err := json.Marshal(userInput(qm))
- 		if err != nil {
- 			fmt.Println(err)
- 		}
- 		_, err = w.Write(data)
- 		if err != nil {
- 			fmt.Println(err)
- 		}
- 	}
- 	http.HandleFunc("/calc", calcHandler)
+		data, err := json.Marshal(userInput(qm, result))
+		if err != nil {
+			fmt.Println(err)
+		}
+		_, err = w.Write(data)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	http.HandleFunc("/calc", calcHandler)
 
 	err := http.ListenAndServe(":8080", nil)
- 	if err != nil {
- 		fmt.Println(err)
+	if err != nil {
+		fmt.Println(err)
 	}
- }
+}
