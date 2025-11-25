17905f3b52f82febad0b2b8b702221f57c3ee9f0
craankism <saschaschwellnus@gmail.com>
2025-11-25 20:05:16 +0100

    fix: add math/big package instead of float64

--- calculate.go
+++ calculate.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/big"
 	"strconv"
 	"strings"
 )
@@ -11,43 +12,49 @@ func calculate(term string) string {
 	fmt.Println(output)
 
 	var stack []string
-	var result float64
+	var result *big.Float
 
 	for i := 0; i < len(output); i++ {
+		if len(output) == 1 {
+			return output[0]
+		}
 		//numbers get added to stack
 		if output[i] != "+" && output[i] != "-" && output[i] != "*" && output[i] != "/" {
 			stack = append(stack, output[i])
 
 			//set operator and calculate
 		} else {
-			//set a and b to last 2 positions stack
-			a, err := strconv.ParseFloat(stack[len(stack)-2], 64)
-			if err != nil {
-				fmt.Println(err)
-			}
-			b, err := strconv.ParseFloat(stack[len(stack)-1], 64)
-			if err != nil {
-				fmt.Println(err)
-			}
+			//string to float64
+			s2, _ := strconv.ParseFloat(stack[len(stack)-2], 64)
+			//float64 to big.Float
+			a := big.NewFloat(s2)
+
+			//String into float64
+			s1, _ := strconv.ParseFloat(stack[len(stack)-1], 64)
+			//float64 to big.Float
+			b := big.NewFloat(s1)
+
+			//set zero to 0 for divide check
+			zero := big.NewFloat(0)
 
 			currentOp := string(output[i])
 
 			switch currentOp {
 			case "+":
-				result = a + b
+				result = new(big.Float).Add(a, b)
 			case "-":
-				result = a - b
+				result = new(big.Float).Sub(a, b)
 			case "*":
-				result = a * b
+				result = new(big.Float).Mul(a, b)
 			case "/":
-				result = a / b
-				if b == 0 {
+				result = new(big.Float).Quo(a, b)
+				if b.Cmp(zero) == 0 {
 					fmt.Println("Err: Can't divide by 0")
 				}
 			}
 			//remove last two numbers from stack and add result to stack
 			stack = stack[:len(stack)-2]
-			stack = append(stack, strconv.FormatFloat(result, 'f', -1, 64))
+			stack = append(stack, result.String())
 		}
 	}
 	if len(output) == 0 && len(stack) == 1 {
@@ -58,51 +65,5 @@ func calculate(term string) string {
 		}
 	}
 	fmt.Println("Result: ", result)
-	return strconv.FormatFloat(result, 'f', -1, 64)
+	return result.String()
 }
-
-/*pop remaining operators from stack (LIFO = Last-In-First-Out)
-for len(stack) > 0 {
-	output = append(output, stack[len(stack)-1])
-	stack = stack[:len(stack)-1]
-}
-*/
-
-/*
-	//call input with numbers and operators from parser
-	rpn := parser(lastTerm)
-	fmt.Println("parser_output: ", rpn)
-
-	//start with first number
-	//result get later updated inside of the loop
-	result, err := strconv.Atoi(numbers[0])
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	//calculate
-	for i := 1; i < len(numbers);  i++ {
-
-		b, err := strconv.Atoi(numbers[i])
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		switch operators[i-1] {
-		case "+":
-			result = result + b
-		case "-":
-			result = result - b
-		case "*":
-			result = result * b
-		case "/":
-			result = result / b
-			if b == 0 {
-				fmt.Println("Err: No division with zero!")
-			}
-		default:
-			fmt.Println("Error! Wrong operator")
-		}
-	}
-	fmt.Println("result: ", result)
-*/
